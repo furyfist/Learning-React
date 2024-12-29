5177a8a7713c0832ce31e8d8c96e1f38daa969ab
Himanshu <himanshu290304@gmail.com>
2024-12-29 12:21:06 +0530

    useref function used

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
-
+import { useRef, useState } from 'react';
 function App(){
     // Function to set focus on input.
+
+    const inputRef = useRef();
+    
     function focusOnInput(){
-        document.getElementById("name").focus()
+        // document.getElementById("name");
+        inputRef.current.focus();
     }
 
     return <div>
         Sign Up
-        <input id="name" type={"text"}></input >
+        <input ref={inputRef} id="name" type={"text"}></input >
         <input type={"}text"}></input >
         <button onClick={focusOnInput} >Submit</button>        
     </div>
 }
 
-export default App
+export default App 
